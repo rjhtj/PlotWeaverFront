e4e9e3155d55225926459662f1b78fe3af08cced
阮剑鸿 <ruanjianhong@ruanjianhongdeMacBook-Pro.local>
2026-02-11 14:23:47 +0800

    调用后端api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-if="loading">加载中...</div>
+    <div v-else-if="error" class="error">请求出错：{{ error }}</div>
+    <div v-else class="result">
+      <pre>{{ apiData }}</pre>
+    </div>
   </div>
 </template>
 
@@ -36,23 +15,49 @@ export default {
   props: {
     msg: String
   }
+  ,data() {
+    return {
+      apiData: null,
+      loading: true,
+      error: null
+    }
+  },
+  mounted() {
+    fetch('/api')
+      .then(res => {
+        if (!res.ok) throw new Error(res.status + ' ' + res.statusText)
+        return res.text().then(text => {
+          try {
+            return JSON.parse(text)
+          } catch (e) {
+            return text
+          }
+        })
+      })
+      .then(data => {
+        this.apiData = data
+        this.loading = false
+      })
+      .catch(err => {
+        this.error = err.message || String(err)
+        this.loading = false
+      })
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  padding: 16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.error {
+  color: #e54d42;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.result {
+  white-space: pre-wrap;
+  background: #f6f8fa;
+  padding: 12px;
+  border-radius: 6px;
+  margin-top: 8px;
 }
 </style>
